e88064ba926e4cdff8dd070f4f70f0a20fb705ad
zahrasarwono <zahrasarwono@localhost>
2024-02-07 14:21:08 -0600

    done

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -1,10 +1,26 @@
 package studio3;
 
+import java.util.Scanner;
+
 public class Sieve {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	Scanner in = new Scanner(System.in);
+	System.out.println("What is our number?");
+	int n = in.nextInt();
+	
+	boolean[]isPrime=new boolean[n+1];
+	for (int i=1; i < isPrime.length; i++)
+	{
+		for (double j=2;j<i;j++)
+		{
+			if (int)(Math,roundi/j 
+		}
+		int x = x+2;
+		isPrime[x] = false;
+		for (intx=)
+	}
 	}
 
 }
